426d104addf531afe20efd1c2f1c510b681d8229
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-09-13 19:55:14 -0400

    First labs for JS

---
+++ JS-LAB-1-IntroToLogic-2022/lab1.js
@@ -0,0 +1,9 @@
+// alert("Hello, World!");
+
+var myAge = 28;
+var daysInYear = 365;
+var ageMultipliedDaysInYear = myAge * daysInYear;
+var firstHalf = "I am";
+var secondHalf = "days old... more or less";
+
+alert(firstHalf + " " + ageMultipliedDaysInYear + " " + secondHalf + "!");
