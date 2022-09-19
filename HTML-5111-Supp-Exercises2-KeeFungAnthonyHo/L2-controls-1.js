501ca5b85d27e5998345937f6ca1f3961eeaba91
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-09-19 09:29:42 -0400

    updated w/ lesson 2, control structures

---
+++ HTML-5111-Supp-Exercises2-KeeFungAnthonyHo/L2-controls-1.js
@@ -0,0 +1,21 @@
+//LESSON 2 - Control Structures
+//alert ('Lesson 2');
+
+var firstName = "Anthony";
+if (firstName == "Anthony") {
+  alert("Hello, Anthony!");
+}
+
+var myAge = 39;
+if (myAge <= 65) {
+  alert("What are you doing at work?");
+}
+
+var loggedIn = 0;
+if (loggedIn == false) {
+  alert("That can't be true!");
+}
+
+if (firstName === "Anthony" || firstName === "Bernie") {
+  alert("Hello, Anthony or Bernie.");
+}
