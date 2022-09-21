11366ae623c8d35c02d2b8d81a48d958cc1d9226
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-09-21 13:32:02 -0400

    SQL lab 2 update

--- SQL-5112-Lab2-KeeFungAnthonyHo/lab2_kee_fung_anthony_ho.sql
+++ SQL-5112-Lab2-KeeFungAnthonyHo/lab2_kee_fung_anthony_ho.sql
@@ -1,18 +1,18 @@
 -- Question 1 --
-SELECT Primary_Offence, NeighbourhoodName
-FROM bikeTheft
-WHERE NeighbourhoodName
-LIKE '%humber%'
+SELECT Primary_Offence, NeighbourhoodName --grabs the two columns
+FROM bikeTheft --from this table
+WHERE NeighbourhoodName --checks within the specific column
+LIKE '%humber%' -- whether the word "Humber" appears within any strings
 
 -- Question 2 --
-SELECT Primary_Offence, NeighbourhoodName, Occurrence_Year 
-FROM bikeTheft 
-WHERE NeighbourhoodName 
-LIKE '%humber%'
-AND MOD (Occurrence_Year, 2) <>0;
+SELECT Primary_Offence, NeighbourhoodName, Occurrence_Year -- grabs the three columns
+FROM bikeTheft  -- from this table
+WHERE NeighbourhoodName -- checks within this column
+LIKE '%humber%' -- whether word 'Humber' appears within any strings
+AND MOD (Occurrence_Year, 2) <>0; -- and has an odd number
 
 -- Question 3 --
-SELECT CONCAT ('$', ROUND(AVG(Cost_of_Bike), 2))
+SELECT CONCAT ('$', ROUND(AVG(Cost_of_Bike), 2)) -- concatonates the $ with [get the rounded average of bike costs by 2 decimals]
 AS 'Average Cost of Stolen Bikes'
 FROM bikeTheft
 
@@ -21,18 +21,19 @@ SELECT COUNT(DISTINCT Bike_Colour, Bike_Speed)
 FROM bikeTheft
 
 -- Question 5 --
-SELECT Location_Type, COUNT(*) AS 'Num of Thefts'
+SELECT Location_Type, COUNT(*) AS 'Num of Thefts' -- gets the data from column and count all rows
 FROM bikeTheft
-GROUP BY Location_Type 
-HAVING COUNT(*) > 500
-ORDER BY COUNT(*) DESC
+GROUP BY Location_Type -- groups the data gathered with the Location_Type column
+HAVING COUNT(*) > 500 -- checks the total counted data for each row within the Location_Type column, and shows anything greater than 500
+ORDER BY COUNT(*) DESC -- displays rows in descending (greatest on top) order
 
 -- Question 6 --
 SELECT MIN(Occurrence_Date) AS 'First Bike Theft in Table'
 FROM bikeTheft
 
 -- Question 7 --
-SELECT NeighbourhoodName AS 'Neighourhood', COUNT(*) AS 'Reported Thefts'
+SELECT NeighbourhoodName AS 'Neighourhood',
+COUNT(*) AS 'Reported Thefts'
 FROM bikeTheft
 GROUP BY NeighbourhoodName 
 ORDER BY COUNT(*) DESC 
@@ -50,3 +51,10 @@ FROM bikeTheft
 -- AVG(COALESCE(Cost_of_Bike, 0)) --
 
 -- Question 10 --
+SELECT NeighbourhoodName AS 'Neighbourhoods',
+COUNT(*) AS 'Incidents Above Average Thefts'
+FROM bikeTheft
+GROUP BY NeighbourhoodName
+ORDER BY COUNT(*) DESC
+LIMIT 5 OFFSET 0
+-- not sure :(
