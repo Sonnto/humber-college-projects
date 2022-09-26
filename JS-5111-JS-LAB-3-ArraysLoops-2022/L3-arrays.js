a022f0f5a6738d0f0b0367a8c1729d1fa1ad09c3
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-09-26 09:02:17 -0400

    lesson update

---
+++ JS-5111-JS-LAB-3-ArraysLoops-2022/L3-arrays.js
@@ -0,0 +1,6 @@
+/* LESSON 3 - ARRAYS */
+var myName = "Anthony";
+var myArray = []; // CREATING AN ARRAY
+
+var myBooks = ["Joy of Cooking", "Joy of Coding", "Joy of Cats"]; //STRINGS
+var myNums = [10, 2, 40, 300]; // NUMBERS
