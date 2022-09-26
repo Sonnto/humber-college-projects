ae698d7f624e11857050f6a250e0f0fe26e9d4f3
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-09-26 09:04:58 -0400

    Update L3-arrays.js

--- JS-5111-JS-LAB-3-ArraysLoops-2022/L3-arrays.js
+++ JS-5111-JS-LAB-3-ArraysLoops-2022/L3-arrays.js
@@ -4,3 +4,5 @@ var myArray = []; // CREATING AN ARRAY
 
 var myBooks = ["Joy of Cooking", "Joy of Coding", "Joy of Cats"]; //STRINGS
 var myNums = [10, 2, 40, 300]; // NUMBERS
+var myStuff = ["hot", 54, myName, true]; //MIXED DATA TYPES
+console.log(myStuff); // VIEW THE ARRAY
