2645759122566133cd9f1bf2e0c148c50c437b67
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-09-28 16:52:03 -0400

    add console.logs, comment out console.logs for finalization

--- JS-5111-Assignment-2-LibraryArray-2022/javascript/script.js
+++ JS-5111-Assignment-2-LibraryArray-2022/javascript/script.js
@@ -22,39 +22,44 @@ bookList[8] = book9;
 bookList[9] = book10;
 
 //LOGIC
-// userInput = prompt("Which top 10 book would you like?", "Pick a number: 1-10");
-// var userChoice = parseInt(userInput) - 1;
-// var bookIndex = bookList.indexOf(bookList[userChoice]);
-// console.log(bookIndex);
 var userInput = prompt(
   "Which top 10 book would you like?",
   "Pick a number: 1-10"
-);
-console.log("passed first userInput prompt"); //ASK USER FOR INPUT
+); //ASK USER FOR INPUT
+// console.log("check; userInput value: " + userInput);
+// console.log("check; passed first userInput prompt");
 let userChoice = parseInt(userInput) - 1; //ENSURES ENTERING 1-10 INPUT IS READ AS 0-9.
 let bookIndex = bookList.indexOf(bookList[userChoice]); //1-10 READ AS 0-9 for n in array.indexOf(array[n]);
-console.log(bookIndex);
+// console.log("check; bookIndex value: " + bookIndex);
 if (isNaN(userInput) || userInput > 10 || userInput <= 0) {
-  // TRIGGERS INVALID INPUT ALERT IF INPUT IS NOT A NUMBER, GREATER THAN 10, OR LESS THAN 0
-  console.log("passed first if-statement");
+  // TRIGGERS INVALID INPUT ALERT IF INPUT IS NOT A NUMBER, NUMBER IS GREATER THAN 10, OR LESS THAN 0
+  // console.log(
+  //   "check; triggered if-statement conditions of userInput being NaN, > 10, <= 0"
+  // );
   alert("Invalid input. Please enter a number between 1 and 10.");
-  console.log("triggered first if-alert");
+  // console.log("check, triggered first if-alert");
 }
 //LOOP START
 while (bookIndex === -1) {
-  // TRIGGERS while LOOP IF bookIndex is -1
+  // TRIGGERS while LOOP IF bookIndex IS -1
+  // console.log("check; entering while-loop userInput prompt");
   userInput = prompt(
     "Which top 10 book would you like?",
     "Pick a number: 1-10"
   ); //ASKS USER FOR INPUT AGAIN
-  console.log("passed userInputprompt inside while-loop");
+  // console.log("check; userInput value: " + userInput);
+  // console.log("check; passed userInput prompt inside while-loop");
   userChoice = parseInt(userInput) - 1;
   bookIndex = bookList.indexOf(bookList[userChoice]);
-  console.log(bookIndex);
+  // console.log("check; bookIndex value: " + bookIndex);
   if (isNaN(userInput) || userInput > 10 || userInput <= 0) {
+    // console.log(
+    //   "check; triggered inside while-loop: if-statement conditions of userInput being NaN, > 10, <= 0"
+    // );
     alert("Invalid input. Please enter a number between 1 and 10.");
-    console.log("triggered if-alert inside while-loop");
+    // console.log("check; triggered if-alert inside while-loop");
   }
+  // console.log("check; leaving while-loop");
 }
 //LOOP END
 alert(
@@ -63,13 +68,13 @@ alert(
     userInput +
     " " +
     "on the list is" +
-    " " +
+    " '" +
     bookList[userChoice] +
-    "."
+    "'."
 );
-console.log("passed final alert with books");
+// console.log("check; passed final alert about selected book");
 for (var i = 0; i < bookList.length; i++) {
   var j = i + 1;
   console.log("Book #" + j + ":" + " " + bookList[i]);
-  console.log("complete");
 }
+// console.log("check; complete");
