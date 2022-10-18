68e543f153c701e4641a52044830c543baed6103
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-10-17 20:17:14 -0400

    added prompts to change object properties

--- JS-5111-Assignment-3-Custom-Object-2022/javascript.js
+++ JS-5111-Assignment-3-Custom-Object-2022/javascript.js
@@ -1,9 +1,9 @@
 //CREATE OBJECT
 let macBookPro = {
-  year: "early 2018",
+  year: 2018,
   ram: "16gb",
   storage: "1tb",
-  displaySize: "16 inches",
+  screenSize: "13inch",
   upgradeInternals: function () {
     macBookPro.ram = "32gb";
     alert(
@@ -11,3 +11,26 @@ let macBookPro = {
     );
   },
 };
+
+//OUTPUT OBJECT INTO CONSOLE
+console.log(macBookPro);
+
+//USER UPGRADES macBookPro.year
+let yearUpgrade = prompt(
+  "Please insert a year to upgrade your MacBook Pro:",
+  macBookPro.year
+);
+macBookPro.year = yearUpgrade;
+console.log(macBookPro);
+
+//USER UPGRADES macBookPro.screenSize
+let screenChoice = prompt(
+  "Please choose a screen size between 13inch or 16inch",
+  macBookPro.screenSize
+);
+macBookPro.screenSize = screenChoice;
+console.log(macBookPro);
+
+//USER GETS FREE UPGRADE OF macBookPro.ram THROUGH APPLE
+macBookPro.upgradeInternals();
+console.log(macBookPro);
